8e89ad299a76d2153b254fac603b780b73611a14
Dima-Vas <dmytro.vasylkiv@gmail.com>
2025-04-04 00:57:49 +0300

    UART : circular buffer for FT232R

--- src/UART.v
+++ src/UART.v
@@ -5,22 +5,30 @@
 // ----------
 module UART #(
     parameter ClockFrequency = 50_000_000,
-    parameter BaudRate = 115200
+    parameter BaudRate = 115200,
+    parameter BufferSize = 2 // must be a power of 2
 )(
     input wire CLK, RST,
-    input wire i_send,
+    input wire i_ready,
     input [7:0] i_frame,
-    output reg o_data
+    output reg o_data,
+    output wire o_ready
 );
 
     localparam FrameWidth = 10;
     localparam TicksPerBit = ClockFrequency / BaudRate;
     
     reg Switch_Sending = 1'b0;
+    
+    reg [7:0] FrameBuffer [BufferSize-1:0]; // circular buffer
+    reg [$clog2(BufferSize)-1:0] Counter_BufferHead;
+    reg [$clog2(BufferSize)-1:0] Counter_BufferTail;
 
     reg [9:0] ShiftRegister = 0;
     reg [$clog2(FrameWidth)-1:0] Counter_CurrRegisterBit = 0;
     reg [$clog2(TicksPerBit)-1:0] Counter_CurrTick = 0;
+    
+    assign o_ready = ((Counter_BufferHead + 1) != Counter_BufferTail);
     
     always @(posedge CLK) begin
         if (!RST) begin        
@@ -29,17 +37,24 @@ module UART #(
             Counter_CurrTick <= 0;
             Switch_Sending <= 0;
             o_data <= 1'b1;
+            Counter_BufferTail <= 0;
+            Counter_BufferHead <= 0;
         end else begin
-            if (i_send && !Switch_Sending) begin
-                ShiftRegister <= {1'b1, i_frame[7:0], 1'b0};
+            if (i_ready && o_ready) begin
+                FrameBuffer[Counter_BufferHead] <= i_frame;
+                Counter_BufferHead <= Counter_BufferHead + 1;
+            end
+            if (!Switch_Sending && (Counter_BufferHead != Counter_BufferTail)) begin
+                ShiftRegister <= {1'b1, FrameBuffer[Counter_BufferTail], 1'b0};
                 Switch_Sending <= 1;
+                Counter_BufferTail <= Counter_BufferTail + 1;
             end else if (Switch_Sending) begin
                 if (Counter_CurrTick < TicksPerBit - 1) begin
                     Counter_CurrTick <= Counter_CurrTick + 1;
                 end else begin // enough ticks, transmit
                     Counter_CurrTick <= 0;
                     o_data <= ShiftRegister[0];
-                    ShiftRegister <= ShiftRegister >> 1;
+                    ShiftRegister <= {1'b1, ShiftRegister[9:1]};
                     Counter_CurrRegisterBit <= Counter_CurrRegisterBit + 1;
                     if (Counter_CurrRegisterBit == FrameWidth - 1) begin
                         Switch_Sending <= 0;
